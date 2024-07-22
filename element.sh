03c25d05f7e0a0134be9c723345a40a7a061ec82
darwin watterson <izimovaan@gmail.com>
2024-07-22 12:16:50 +0000

    feat: compare passed argument with table data

--- element.sh
+++ element.sh
@@ -7,10 +7,19 @@ then
   echo Please provide an element as an argument.
 elif [[ $1 =~ ^[0-9]+$ ]]
 then
-  echo "It's a number"
-elif [[ ! $1 =~ ^[0-9]+$ ]]
-then
-  echo "It's a string"
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo "The element with atomic number $ATOMIC_NUMBER"
+  fi
 else
-  echo I could not find that element in the database.
+  NAME=$($PSQL "SELECT name FROM elements WHERE name='$1' OR symbol='$1'")
+  if [[ -z $NAME ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo "The element with name '$NAME'"
+  fi
 fi
