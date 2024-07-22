df3364a055aeb8709f6e751a5f132ca5942703d8
darwin watterson <izimovaan@gmail.com>
2024-07-22 11:59:44 +0000

    feat: check the passed argument

--- element.sh
+++ element.sh
@@ -5,4 +5,12 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
+elif [[ $1 =~ ^[0-9]+$ ]]
+then
+  echo "It's a number"
+elif [[ ! $1 =~ ^[0-9]+$ ]]
+then
+  echo "It's a string"
+else
+  echo I could not find that element in the database.
 fi
